6eeb7f2e4e7bf9dfe36d8429d064fc9d0670244d
Dmytro Murashko <riddon04@gmail.com>
2020-11-25 21:54:32 +0200

    Added Pagination

--- src/components/Layouts/MainPage/MainPage.js
+++ src/components/Layouts/MainPage/MainPage.js
@@ -1,25 +1,46 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import PhotoList from "../../DataDisplay/PhotoList/PhotoList";
 import {getPhotoList} from "../../../services/photoGalleryAPI";
 import * as photoGalleryActions from "../../../actions/photoGalleryActions";
+import PaginationBlock from "../PaginationBlock/PaginationBlock";
 
 const MainPage = () => {
 
     const photoList = useSelector(state => state.photoGalleryReducer.photoList);
     const dispatch = useDispatch();
+    const [photoPage, setPhotoPage] = useState(1);
+    const [photoAmount, setPhotoAmount] = useState(10);
 
     useEffect(() => {
-        getPhotoList(1, 10).then((result) => {
+        getPhotoList(photoPage, photoAmount).then((result) => {
             console.log('Result -->', result.data);
 
             dispatch(photoGalleryActions.getList(result.data));
         })
-    }, [dispatch]);
+    }, [dispatch, photoPage, photoAmount]);
+
+    const onChangePage = (val) => {
+        if (val === 'prev' && photoPage > 1) {
+            setPhotoPage(photoPage - 1);
+        } else if(val === 'next') {
+            setPhotoPage(photoPage + 1);
+        }
+    };
+
+    const onChangeAmount = (val) => {
+        setPhotoAmount(val);
+    };
 
     return (
         <>
             <div className="container">
+                <PaginationBlock
+                    onChangePage={onChangePage}
+                    photoPage={photoPage}
+                    photoAmount={photoAmount}
+                    onChangeAmount={onChangeAmount}
+                />
                 <PhotoList
                     photoList={photoList}
                 />
---
+++ src/components/Layouts/PaginationBlock/PaginationBlock.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './PaginationBlock.scss';
+
+const PaginationBlock = ({onChangePage, photoPage, photoAmount, onChangeAmount}) => {
+
+    const moveToNextPage = () => {
+        onChangePage('next')
+    };
+
+    const moveToPrevPage = () => {
+        onChangePage('prev')
+    };
+
+    const onChangePhotoAmount = (e) => {
+        onChangeAmount(e.target.value);
+    };
+
+    return (
+        <>
+            <div className="pagination">
+                <div className="pagination__btn-block">
+                    <button
+                        className="pagination__btn--prev pagination__btn"
+                        onClick={moveToPrevPage}
+                        disabled={(photoPage === 1)}
+                    >
+                        prev
+                    </button>
+                    <button
+                        className="pagination__btn--next pagination__btn"
+                        onClick={moveToNextPage}
+                    >
+                        next
+                    </button>
+                </div>
+
+                <form className="pagination__form">
+                    <label className="pagination__photo-amount-title" htmlFor="photo-amount">
+                        Photo amount
+                    </label>
+                    <select className="pagination__photo-amount" name="photo-amount" value={photoAmount} onChange={onChangePhotoAmount}>
+                        <option value={10}>10</option>
+                        <option value={20}>20</option>
+                        <option value={30}>30</option>
+                    </select>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default PaginationBlock;
