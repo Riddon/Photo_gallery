58a54e4616d985723adbc6d0cdcef191efd17284
Dmytro Murashko <riddon04@gmail.com>
2020-11-26 23:10:25 +0200

    Cleaned notused constants and photoGalleryAPI

--- src/constants/constURL.js
+++ src/constants/constURL.js
@@ -1,2 +1,1 @@
 export const photoListURL = "https://picsum.photos/v2/list";
-export const photoInfoURL = "https://picsum.photos/id";
--- src/services/photoGalleryAPI.js
+++ src/services/photoGalleryAPI.js
@@ -1,20 +1,10 @@
 import axios from 'axios';
-import {photoListURL, photoInfoURL} from '../constants/constURL';
+import {photoListURL} from '../constants/constURL';
 
 export const getPhotoList = (page, limits) => {
     try {
         return axios
             .get(`${photoListURL}?page=${page}&limit=${limits}`)
-            .then(res => res);
-    } catch (error) {
-        return error.response;
-    }
-};
-
-export const getPhoto = async (id) => {
-    try {
-        return await axios
-            .get(`${photoInfoURL}/${id}/info`)
             .then(res => res);
     } catch (error) {
         return error.response;
